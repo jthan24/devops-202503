76b475a0f5c16c3e2c5a7a2dc69bd079bae84e04
Miguel Herrera <miguel.herrera@petco.com>
2025-11-20 18:16:38 -0500

    Miguel PR

---
+++ quien-es-rico.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Created by: Miguel Herrera
+
+nombre = input("Ingresa tu nombre: ")
+edad = input("Ingresa tu edad: ")
+
+print(f"Hola, {nombre} tu tienes {edad} años y eres rico.")
