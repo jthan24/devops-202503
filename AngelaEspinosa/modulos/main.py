6417f464ed70c5903cbf7d075916a6a7cfe21786
Angela Espinosa <titagena@gmail.com>
2025-11-26 22:01:05 -0500

    agregamos con exito el proceso de multiplicación y división a mi modulo

---
+++ AngelaEspinosa/modulos/main.py
@@ -0,0 +1,12 @@
+import mimodulo
+
+# from mimodulo import suma
+
+input_a = input("Ingresa el numero a :")
+input_b = input("Ingresa el numero b :")
+
+a = int(input_a)
+b = int(input_b)
+
+print(mimodulo.suma(a, b))
+print(mimodulo.resta(a, b))
