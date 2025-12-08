7bdb9bf4f5d2a20d0bbc975a90567bb9b7b2af24
Luis Sebastian Ibarra <luissebastianibarrav@gmail.com>
2025-12-08 16:15:37 -0500

    feat: add modulos python

---
+++ modulos/main.py
@@ -0,0 +1,12 @@
+import mimodulo
+
+input_a =  input("Ingrese el primer número: ")
+input_b =  input("Ingrese el segundo número: ")
+
+a =  int(input_a)
+b =  int(input_b)
+
+print(mimodulo.suma(a, b))
+print(mimodulo.resta(a, b))
+print(mimodulo.multiplicacion(a, b))
+print(mimodulo.division(a, b))
